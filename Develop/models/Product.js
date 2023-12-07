bdc7109b3fca30a13a5b9570d046ae0177fca66f
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-06 20:20:55 -0600

    models/products.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,8 +8,54 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+   // define columns
   {
-    // define columns
+   id: {
+    // Sets data type to integer
+    type: DataTypes.INTEGER,
+    // Bans NULL values
+    allowNull: false,
+    // Set as primary key
+    primaryKey: true,
+    // Increments the value for each new record
+    autoIncrement: true,
+   },
+   product_name: {
+    // Sets data type to STRING
+    type: DataTypes.STRING,
+    //Bans NULL values
+    allowNull: false,
+   },
+   price: {
+    // Sets data type to decimal number
+    type: DataTypes.DECIMAL,
+    // Bans NULL values
+    allowNull: false,
+    validate: {
+      // Validate the value is decimal
+      isDecimal: true,
+    },
+   },
+   stock: {
+    // Sets data type to integer
+    type: DataTypes.INTEGER,
+    // Bans NULL values
+    allowNull: false,
+    // Sets default value to 10
+    defaultValue: 10,
+    validate: {
+      // Validate the value is numeric
+      isNumeric: true,
+    },
+   },
+   category_id: {
+    // Sets data type to integer
+    type: DataTypes.INTEGER,
+    references: {
+      // Reference category table
+      model: "category",
+    },
+   },
   },
   {
     sequelize,
