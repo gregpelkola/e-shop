fe14f7683ffd51add79528bf4442fb11ed67c198
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-06 20:29:51 -0600

    models/producttag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,36 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      // Set the data type to integer
+      type: DataTypes.INTEGER,
+      // Bans NULL values
+      allowNull: false,
+      // Sets the primary key
+      primaryKey: true,
+      // Increments the value for each new record
+      autoIncrement: true,
+    },
+    tag_id: {
+      // Set the data type to integer
+      type: DataTypes.INTEGER,
+      references: {
+        // Reference the tag table
+        model: "tag",
+        //reference the id column in the tag table
+        key: "id",
+      },
+    },
+    product_id: {
+      // Set data type to integer
+      type: DataTypes.INTEGER,
+      references: {
+        // Reference the product table
+        model: "product",
+        // Reference the the id column in the product table
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
