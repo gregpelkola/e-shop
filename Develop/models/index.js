2c8267f436bab882773e2c9c29978c0b77e6d2f0
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-06 20:07:26 -0600

    models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // Foreign key in Product model
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // Foreign key in the Product model
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Intermediate model
+  through: ProductTag,
+  // Foreign key in the Product model
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Foreign key in the Product Model
+  foreignKey: 'category_id',
+});
 
 module.exports = {
   Product,
