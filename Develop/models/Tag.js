fdfeef6509d72df5e1f5e9eb00b224910713aa0e
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-06 20:33:52 -0600

    models/tag.js

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      // Set the data type to integer
+      type: DataTypes.INTEGER,
+      // Bans NULL values
+      allowNull: false,
+      // Set the primary key
+      primaryKey: true,
+      // Increments the value for each new record
+      autoIncrement: true,
+    },
+    tag_name: {
+      // Set the data type to string
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
